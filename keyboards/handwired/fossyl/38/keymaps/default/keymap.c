fcbc2cfcac44e1a5384b4ed16fe7aa748f796311
YmgtYshr <y.yasuhiro0113@gmail.com>
2022-05-22 06:14:37 +0900

    fixed codes for 38

--- keyboards/handwired/fossyl/38/keymaps/default/keymap.c
+++ keyboards/handwired/fossyl/38/keymaps/default/keymap.c
@@ -3,6 +3,7 @@
 #define _BASE 0
 #define _LOWER 1
 #define _RAISE 2
+#define _ADJUST 3
 
 #define SFT_ESC  SFT_T(KC_ESC)
 #define SFT_TAB  SFT_T(KC_TAB)
@@ -28,6 +29,10 @@
 #define LOWER1 LT(_LOWER,KC_N)
 #define RAISE2 LT(_RAISE,KC_Z)
 #define LOWER2 LT(_LOWER,KC_SLSH)
+
+#define ADJ_R LT(_ADJUST,KC_ENT)
+#define ADJ_L LT(_ADJUST,KC_SPC)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base (qwerty)
@@ -63,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN2, KC_MINS, KC_DEL,
         SFT_MHEN, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,             KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_PLUS, SFT_HENK,
         _______, KC_UNDS, KC_PIPE, KC_MINS, KC_UNDS, KC_BSLS,             KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L, KC_EQL,  _______,
-                                                     _______,            _______
+                                                     ADJ_L,               _______
     ),
     /* RAISE
      * +-----------------------------------------+                             +-----------------------------------------+
@@ -77,9 +82,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                      +--------------------+                             +--------------------+
      */
     [_RAISE] = LAYOUT(
-        RESET,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         SFT_MHEN, KC_GRV,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, SFT_HENK,
         _______, KC_EXLM, KC_QUES, KC_TILD, KC_LPRN, KC_LBRC,             KC_RBRC, KC_RPRN, _______, _______, KC_EQL,  _______,
+                                                     _______,            ADJ_R
+    ),
+    /* ADJUST
+     * +-----------------------------------------+                             +-----------------------------------------+
+     * | ECS  |   1  |   2  |   3  |   4  |   5  |                             |   6  |   7  |   8  |   9  |   0  | BSPC |
+     * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+     * | CTL  |   `  |   @  |   #  |   $  |   %  |                             | LEFT | DOWN |  UP  |RIGHT |   ;  |  '   |
+     * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+     * | SHFT |   !  |   ?  |   ~  |   (  |   [  |                             |   ]  |   )  |   ,  |   .  |   =  | SHFT |
+     * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
+     *                      | BSPC |RAISE | OPT  |                             | CMD  |LOWER | SPC  |
+     *                      +--------------------+                             +--------------------+
+     */
+    [_ADJUST] = LAYOUT(
+        KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,
+        SFT_MHEN, _______,  _______,   _______, _______,  _______,             _______, _______, _______,   _______, _______, SFT_HENK,
+        _______, _______, _______, _______, _______, RESET,             _______, _______, _______, _______, _______,  _______,
                                                      _______,            _______
     )
 };
