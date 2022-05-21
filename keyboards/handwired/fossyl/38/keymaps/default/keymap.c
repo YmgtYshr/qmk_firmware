bc8ebd4c48cd8632e49b17fd62d78047f90e443e
YmgtYshr <y.yasuhiro0113@gmail.com>
2022-05-22 06:33:03 +0900

    fixed codes for 38

--- keyboards/handwired/fossyl/38/keymaps/default/keymap.c
+++ keyboards/handwired/fossyl/38/keymaps/default/keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                      +--------------------+                             +--------------------+
      */
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        SFT_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SFT_QUOT,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+        SFT_MHEN, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SFT_QUOT,
         _______, RAISE2,    LCTL_T(KC_X),    LOPT_T(KC_C),    LCMD_T(KC_V),    RAISE1,              LOWER1,   RCMD_T(KC_M),    ROPT_T(KC_COMM), RCTL_T(KC_DOT),  LOWER2, _______,
                                                      RAISE,            LOWER
     ),
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                      +--------------------+                             +--------------------+
      */
     [_LOWER] = LAYOUT(
-        _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN2, KC_MINS, KC_DEL,
-        SFT_MHEN, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,             KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_PLUS, SFT_HENK,
+        KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN2, KC_MINS, KC_DEL,
+        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,             KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_PLUS, SFT_HENK,
         _______, KC_UNDS, KC_PIPE, KC_MINS, KC_UNDS, KC_BSLS,             KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L, KC_EQL,  _______,
                                                      ADJ_L,               _______
     ),
@@ -82,8 +82,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                      +--------------------+                             +--------------------+
      */
     [_RAISE] = LAYOUT(
-        _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        SFT_MHEN, KC_GRV,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, SFT_HENK,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        _______, KC_GRV,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, SFT_HENK,
         _______, KC_EXLM, KC_QUES, KC_TILD, KC_LPRN, KC_LBRC,             KC_RBRC, KC_RPRN, _______, _______, KC_EQL,  _______,
                                                      _______,            ADJ_R
     ),
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_ADJUST] = LAYOUT(
         KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,
-        SFT_MHEN, _______,  _______,   _______, _______,  _______,             _______, _______, _______,   _______, _______, SFT_HENK,
+        _______, _______,  _______,   _______, _______,  _______,             _______, _______, _______,   _______, _______, SFT_HENK,
         _______, _______, _______, _______, _______, RESET,             _______, _______, _______, _______, _______,  _______,
                                                      _______,            _______
     )
